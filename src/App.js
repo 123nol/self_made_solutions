23a27107eaed9be2cc07ebbe1cbc8eb2a35e1570
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-09-18 16:57:18 +0300

    NavBar changed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import {BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Shared from "./pages/Shared";
 import Home from "./pages/Home";
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Shared/>}>
-        <Route index element={<Home/>}/>
-
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Shared />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,29 +1,41 @@
-const Navbar = () => {
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+
+const NavBar = () => {
+  const [clicked, setClick] = useState(false);
+
   return (
-    <nav className="navbar">
-      <div className="container">
-        <div className="links">
-          <ul>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/products">Products</a>
-            </li>
-            <li>
-              <a href="/clients">Partners</a>
-            </li>
-            <li>
-              <a href="/contact">Contact</a>
-            </li>
-          </ul>
-        </div>
+    <div className="bar">
+      <div className="banner">Self Made Solutions</div>
+      <nav id="nav" className={clicked ? "#nav active" : "#nav"}>
+        <NavLink to="/" className="links">
+          HOME
+        </NavLink>
+        <NavLink to="/about" className="links">
+          ABOUT
+        </NavLink>
+        <NavLink to="/contacts" className="links">
+          SERVICES
+        </NavLink>
+        <NavLink to="/contacts" className="links">
+          PROJECTS
+        </NavLink>
+        <NavLink to="/contacts" className="links">
+          CONTACT
+        </NavLink>
+      </nav>
+
+      <div
+        className="burger"
+        onClick={() => {
+          setClick(!clicked);
+        }}
+      >
+        <FaBars />
       </div>
-    </nav>
+    </div>
   );
 };
 
-export default Navbar;
+export default NavBar;
