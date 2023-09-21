6643ca4a7c5d655b4f8056933f253a8625057285
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-09-21 18:01:05 +0300

    added cards to the product list and also made it clickable (only the first one)

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,13 +1,87 @@
 const Product = () => {
+  const redirect = () => {
+    window.location.href = "https:youtube.com/";
+  };
   return (
-    <div style={{ height: "100vh" }}>
-      <h1>Products</h1>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Assumenda
-        nesciunt quo dolorem sit porro, quasi, placeat sequi eos repudiandae
-        nemo eveniet ipsum laborum dignissimos a rem culpa voluptate magnam
-        cupiditate.
-      </p>
+    <div className="products">
+      <div className="linkHeader">
+        <h1>Products</h1>
+        <p>Some of the Products that we have worked on</p>
+      </div>
+      <div className="productList">
+        <div className="productCard" onClick={redirect}>
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+        <div className="productCard">
+          <div>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam,
+            voluptatem mollitia modi impedit perspiciatis cum culpa recusandae
+            soluta eligendi temporibus consectetur ipsum praesentium quas
+            architecto nulla, harum quos distinctio corrupti.
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
