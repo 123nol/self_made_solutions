228ed9c22a5be32d2676d7affc47d226f26c14b4
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-09-25 11:22:15 +0300

    nothing changed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,25 +3,27 @@ import groupPic from "../components/images/people.svg";
 import { Link } from "react-router-dom";
 const Home = () => {
   return (
-    <div className="home">
-      <div className="homeChild">
-        <div>
-          <h1>
-            Welcome to <span>Self Made Solutions</span>
-          </h1>
-          <p>Always Striving Excellence</p>
+    <div className="main">
+      <div className="home">
+        <div className="homeChild">
           <div>
-            <Link className="redirectLink" to="/services">
-              <button>View Services</button>
-            </Link>
-            <Link className="redirectLink" to="/product">
-              <button>View Products</button>
-            </Link>
+            <h1>
+              Welcome to <span>Self Made Solutions</span>
+            </h1>
+            <p>Always Striving Excellence</p>
+            <div>
+              <Link className="redirectLink" to="/services">
+                <button>View Services</button>
+              </Link>
+              <Link className="redirectLink" to="/product">
+                <button>View Products</button>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="homeChild imageContainer">
-        <img src={groupPic} alt="" />
+        <div className="homeChild imageContainer">
+          <img src={groupPic} alt="" />
+        </div>
       </div>
     </div>
   );
