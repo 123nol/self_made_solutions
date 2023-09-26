fbe0203db580cba44c136c3c6e6f91f5fc99d50b
Nolawi Assefa <nmerkebe@gmail.com>
2023-09-26 12:27:11 +0300

    modified services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,9 @@
 // import { Link } from "react-router-dom";
+import { useState } from "react";
 const Services = () => {
+  const [orderERP, setOrderERP] = useState(1)
+  const [orderCons, setOrderCons] = useState(1)
+  const [orderDig, setOrderDig] = useState(1)
   return (
     <div className="services">
       <div className="linkHeader">
@@ -14,18 +18,37 @@ const Services = () => {
             <div className="serviceInfo">
               <h3>Service Includes</h3>
               <p>
-                <li>Therapy</li>
-                <li>Cheese</li>
-                <li>System</li>
-                <li>Management</li>
+                <li onClick={()=>setOrderERP(1)} style={{cursor:"pointer"}}>Therapy</li>
+                <li onClick={()=>setOrderERP(2) } style={{cursor:"pointer"}}>Cheese</li>
+                <li onClick={()=>setOrderERP(3)} style={{cursor:"pointer"}}>System</li>
+                <li onClick={()=>setOrderERP(4)} style={{cursor:"pointer"}}>Management</li>
               </p>
             </div>
-
-            <div className="serviceExplanation">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-              deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
-              soluta natus qui id perferendis minima expedita esse quisquam
-              porro libero nisi eveniet quae?
+            <div className="containerserve"style={{width:"100%", overflow:"none"}}>
+              <div className={orderERP===1 ? "serviceExplanation grow" : "serviceExplanation " }>
+                1,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderERP===2 ? "serviceExplanation grow" : "serviceExplanation " }>
+                2,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderERP===3 ? "serviceExplanation grow" : "serviceExplanation " }>
+                3,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderERP===4 ? "serviceExplanation grow" : "serviceExplanation " }>
+                4,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
             </div>
           </div>
           <div className="button">
@@ -39,20 +62,38 @@ const Services = () => {
           <div className="serviceType">
             <div className="serviceInfo">
               <h3>Service Includes</h3>
-
               <p>
-                <li>Therapy</li>
-                <li>Cheese</li>
-                <li>System</li>
-                <li>Management</li>
+                <li onClick={()=>setOrderCons(1)} style={{cursor:"pointer"}}>Therapy</li>
+                <li onClick={()=>setOrderCons(2) } style={{cursor:"pointer"}}>Cheese</li>
+                <li onClick={()=>setOrderCons(3)} style={{cursor:"pointer"}}>System</li>
+                <li onClick={()=>setOrderCons(4)} style={{cursor:"pointer"}}>Management</li>
               </p>
             </div>
-
-            <div className="serviceExplanation">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-              deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
-              soluta natus qui id perferendis minima expedita esse quisquam
-              porro libero nisi eveniet quae?
+            <div style={{width:"100%", overflow:"none"}}>
+              <div className={orderCons===1 ? "serviceExplanation grow" : "serviceExplanation " }>
+                1,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderCons===2 ? "serviceExplanation grow" : "serviceExplanation " }>
+                2,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderCons===3 ? "serviceExplanation grow" : "serviceExplanation " }>
+                3,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderCons===4 ? "serviceExplanation grow" : "serviceExplanation " }>
+                4,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
             </div>
           </div>
           <div className="button">
@@ -66,20 +107,38 @@ const Services = () => {
           <div className="serviceType">
             <div className="serviceInfo">
               <h3>Service Includes</h3>
-
               <p>
-                <li>Therapy</li>
-                <li>Cheese</li>
-                <li>System</li>
-                <li>Management</li>
+                <li onClick={()=>setOrderDig(1)} style={{cursor:"pointer"}}>Therapy</li>
+                <li onClick={()=>setOrderDig(2) } style={{cursor:"pointer"}}>Cheese</li>
+                <li onClick={()=>setOrderDig(3)} style={{cursor:"pointer"}}>System</li>
+                <li onClick={()=>setOrderDig(4)} style={{cursor:"pointer"}}>Management</li>
               </p>
             </div>
-
-            <div className="serviceExplanation">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-              deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
-              soluta natus qui id perferendis minima expedita esse quisquam
-              porro libero nisi eveniet quae?
+            <div style={{width:"100%", overflow:"none"}}>
+              <div className={orderDig===1 ? "serviceExplanation grow" : "serviceExplanation " }>
+                1,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderDig===2 ? "serviceExplanation grow" : "serviceExplanation " }>
+                2,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderDig===3 ? "serviceExplanation grow" : "serviceExplanation " }>
+                3,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
+              <div className={orderDig===4 ? "serviceExplanation grow" : "serviceExplanation " }>
+                4,Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
+                deserunt consequatur quasi reprehenderit enim culpa, voluptatibus
+                soluta natus qui id perferendis minima expedita esse quisquam
+                porro libero nisi eveniet quae?
+              </div>
             </div>
           </div>
         </div>
