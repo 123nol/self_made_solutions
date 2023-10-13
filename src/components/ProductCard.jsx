0fc4c98b87758d635fce5c3c7fde2b24f0876d6c
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-10-13 12:52:10 +0300

    product link added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,10 +3,7 @@ import { Link } from "react-router-dom";
 const ProductCards = ({ data }) => {
   return (
     <div className="productCard">
-      <Link
-        className="redirectLink"
-        to={"https://www.youtube.com/watch?v=dQw4w9WgXcQ"}
-      >
+      <Link className="redirectLink" to={data.link}>
         <div className="productInfo">
           <h1>{data.title}</h1>
           <p>{data.details}</p>
--- src/data/ProductData.jsx
+++ src/data/ProductData.jsx
@@ -3,55 +3,63 @@ export const ProductData = [
     id: 1,
     title: "Therapy",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://airtable.com/",
   },
   {
     id: 2,
     title: "Therapy2",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://youtube.com/",
   },
   {
     id: 3,
     title: "Therapy3",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://youtube.com/",
   },
   {
     id: 4,
     title: "Therapy4",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://youtube.com/",
   },
   {
     id: 5,
     title: "Therapy5",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://airtable.com/",
   },
   {
     id: 6,
     title: "Therapy6",
     details:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://airtable.com/",
   },
   {
     id: 7,
     title: "Therapy7",
     details:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://airtable.com/",
   },
   {
     id: 8,
     title: "Therapy8",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://airtable.com/",
   },
   {
     id: 9,
     title: "Therapy9",
     details:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia fugit tenetur eum voluptatum laudantium a dolorum aperiam corporis, ad architecto provident minima, maxime quod, sequi possimus rem neque! Sunt, itaque?",
+    link: "https://airtable.com/",
   },
 ];
-// export default ProductData;
