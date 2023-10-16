d6156f93682b2263ad89194ad79faa00b0788afb
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-10-16 09:09:32 +0300

    error fixed

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -88,16 +88,3 @@ export const ProjectData = [
       "currently in development An enterprise resource management system  to control and maintain their real estate developments and sales.The system is designed for office use only using the Laravel     framwork for PHP and Tailwind CSS for the front-end development.",
   },
 ];
-
-import React from 'react'
-
-const ProjectData = [
-  {
-    id:1,
-    title: "Nebret Property Management - Brand Logo",
-    img: "https://images.pexels.com/photos/15745221/pexels-photo-15745221/free-photo-of-fog-over-water.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    detail: "Along with the logo design, we were tasked to design and develop an easy-to-use website application that delivers on three basic requirements: Present the firm's listings clearly, that outstands the views and property's features. Create an interactive system that clearly serves the needs of different user types.Develop a system optimized to suite current communication standards of Ethiopia and still suffices for the World.The application is developed using Laravel framework and Bootstrap scafolding.Checkout the website here ..",
-  }
-
-]
-export default ProjectData
