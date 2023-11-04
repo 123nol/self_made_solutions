beb6e6e458a2fda2991aeaee4134253b318535f1
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-11-04 10:14:36 +0300

    fixed nolawi error

--- src/components/NavContext.jsx
+++ src/components/NavContext.jsx
@@ -1,13 +1,13 @@
-import React, { createContext } from 'react'
-import { useContext } from 'react'
-import { useState } from 'react';
+import React, { createContext } from "react";
+import { useContext } from "react";
+import { useState } from "react";
 export const NavContext = createContext();
 
-export const NavContextProvider = ({children}) => {
-  const [order, setOrder] = useState(1)
+export const NavContextProvider = ({ children }) => {
+  const [order, setOrder] = useState(1);
   return (
-    <NavContextProvider value={[order,setOrder]}>
+    <NavContextProvider value={[order, setOrder]}>
       {children}
     </NavContextProvider>
-  )
-}
+  );
+};
