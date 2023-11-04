2f524fcd1975cc321c25251f5306589c9eef000f
maki-ni <114020856+maki-ni@users.noreply.github.com>
2023-11-04 16:31:01 +0300

    everything is over

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ function App() {
   return (
     <>
       <ParticleBackground />
-
       <BrowserRouter>
         <Routes>
           <Route
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router-dom";
-
+import Popup from "./Popup";
+import { useState } from "react";
 const ProductCards = ({ data }) => {
+  const [trigger, setTrigger] = useState(false);
   return (
     <div
       className="productCard"
       style={{ backgroundImage: `url(${data.backgroundImage})` }}
     >
-      <Link className="redirectLink" to={data.link}>
+      <Popup trigger={trigger} setTrigger={setTrigger}>
+        <h3>{data.title}</h3>
+        <p>{data.details}</p>
+      </Popup>
+      <Link className="redirectLink" onClick={() => setTrigger(true)}>
         <div className="productInfo">
           <h1>{data.title}</h1>
           <p>{data.details}</p>
