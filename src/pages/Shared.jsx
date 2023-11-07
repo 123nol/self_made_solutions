4d906d523821e986ef9842c85efe1b3eab36a7c8
Nolawi Assefa <nmerkebe@gmail.com>
2023-11-07 14:24:10 +0300

    something changed

--- src/pages/Shared.jsx
+++ src/pages/Shared.jsx
@@ -6,12 +6,24 @@ import Chat from "../components/Chat.jsx";
 import { useState } from "react";
 
 const Shared = (props) => {
-  const [reach, setReach] = useState(false)
- 
+  const[scroll,setScroll]=useState(0)
+  const handleScroll = (event) => {
+    setScroll(event.currentTarget.scrollTop);
+    if (scroll <=400 ) {
+      return props.change(1);
+    } 
+    else if(scroll<=800){
+      return props.change(2);
+
+    }
+    else if(scroll>=1100){
+      return props.change(3);
+    }
+  };
   return (
-    <div >
+    <div  onScroll={handleScroll} >
       <NavBar order={props.order} change={(order) => props.change(order)} />
-      <Outlet />
+      <Outlet/>
       <Chat/>
       <Footer />
     </div>
