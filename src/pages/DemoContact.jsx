8da3118533779d304485c4b49cdfc2910f470e0c
Nolawi Assefa <nmerkebe@gmail.com>
2023-11-07 20:13:44 +0300

    fixed navbar color and the psedo element after on the links

--- src/pages/DemoContact.jsx
+++ src/pages/DemoContact.jsx
@@ -6,7 +6,7 @@ const DemoContact = () => {
 
   return (
     <div className='demoallcontacts'>
-      <div className='linkHeader' style={{marginTop:"0px"}}>
+      <div className='linkHeader' >
         <h1>Contact</h1>
         <p>Reach us</p>
       </div>
